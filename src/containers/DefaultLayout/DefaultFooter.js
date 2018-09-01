f684dabbc2a20accfe17ed979ce06d91d09ea1c7
Bin Xu <beesheer@gmail.com>
2018-09-01 10:31:14 -0400

    Navigation changes.

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -15,8 +15,8 @@ class DefaultFooter extends Component {
 
     return (
       <React.Fragment>
-        <span><a href="https://coreui.io">CoreUI</a> &copy; 2018 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
+        <span><a href="/#about">Beesheer</a></span>
+        <span className="ml-auto"><a href="/#login">Login</a></span>
       </React.Fragment>
     );
   }
